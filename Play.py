bb2d473fd2927e658b7527a5cb05158ab72b6592
noobie375@live.com <noobie375@live.com>
2016-12-27 04:37:32 -0500

    Play class no longer exists and is now a function of UserInput

--- Play.py
+++ Play.py
@@ -1,11 +1,3 @@
 from UserInput import UserInput
-class Play(UserInput):
-    def play_game(self):
-        print("You are playing X's and O's")
-        self.print_board()
-        print("This is the board, E repersents an empty space")
-        print("use the words top, bottom, left, right, and middle to place you piece")
-        self.game_setup()
-        self.cycle_through_turns()
-game = Play()
+game = UserInput()
 game.play_game()
--- UserInput.py
+++ UserInput.py
@@ -66,3 +66,10 @@ class UserInput(GameEngine):
         print("If you would like to save this game type 'yes'")
         if (input("type yes: ").lower() == "yes"):
             self.save_game(input("Type name of file here: "))
+    def play_game(self):
+        print("You are playing X's and O's")
+        self.print_board()
+        print("This is the board, E repersents an empty space")
+        print("use the words top, bottom, left, right, and middle to place you piece")
+        self.game_setup()
+        self.cycle_through_turns()
